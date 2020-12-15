fefc57bc0b831a6cdbe2080f4c1b3cfd8a1efb5c
Tim MESSAOUDENE <messaoudene-tim@outlook.fr>
2020-12-15 14:13:28 +0100

    mise à jour des fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use App\Entity\Booking;
 use Faker\Factory;
 use App\Entity\Role;
 use App\Entity\User;
@@ -102,6 +103,32 @@ class AppFixtures extends Fixture
 
                 $manager->persist($image);
             }
+
+            // Gestion des réservations
+            for ($k=1; $k <= mt_rand(0, 10) ; $k++) { 
+                $booking = new Booking();
+
+                $createdAt = $faker->dateTimeBetween('-6 months');
+                $startDate = $faker->dateTimeBetween('-3 months');
+
+                $duration = mt_rand(3, 10);
+
+                $endDate = (clone $startDate)->modify("+$duration days");
+                $amount = $ad->getPrice() * $duration;
+
+                $booker = $users[mt_rand(0, count($users) -1)];
+
+                $booking->setBooker($booker)
+                        ->setAd($ad)
+                        ->setStartDate($startDate)
+                        ->setEndDate($endDate)
+                        ->setCreatedAt($createdAt)
+                        ->setAmount($amount);
+
+                $manager->persist($booking);
+
+            }
+
             $manager->persist($ad);
         }
     $manager->flush();
